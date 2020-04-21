cd438dd98f5ac2f827379a3367b025be35d282e1
orzech <orzech@DESKTOP-12QTS4H>
2020-04-21 23:36:39 +0200

    doing tutorial

---
+++ EcsTests/Assets/Scripts/Testing.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        var entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+        entityManager.CreateEntity();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
